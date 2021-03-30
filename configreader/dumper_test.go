0c42de54e9423f1594b2be9a26eda54a2289b9a9
Ben Levy <ben.levy@twosigma.com>
2021-03-29 22:00:48 -0400

    Add tests to exercise FromFile and {Read,Parse}EnvoyConfig

---
+++ configreader/dumper_test.go
@@ -0,0 +1,52 @@
+package configreader
+
+import (
+	"context"
+	"fmt"
+	"os/exec"
+	"testing"
+	"time"
+
+	v3 "github.com/envoyproxy/go-control-plane/envoy/config/bootstrap/v3"
+)
+
+func TestDumper(t *testing.T) {
+	type dumperTest struct {
+		inputFile      string
+		expectedOutput *v3.Bootstrap
+	}
+	tests := map[string]dumperTest{
+		"basic": {
+			inputFile:      "../testdata/static.yaml",
+			expectedOutput: staticBootstrap,
+		},
+	}
+	for testName, test := range tests {
+		t.Run(testName, func(t *testing.T) {
+			ctx, cancelFunc := context.WithCancel(context.Background())
+			defer cancelFunc()
+			cmd := exec.CommandContext(ctx, "envoy", "-c", test.inputFile)
+			go func() {
+				output, err := cmd.CombinedOutput()
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Println(string(output))
+			}()
+			time.Sleep(time.Second)
+			configDump, err := ReadEnvoyConfig("http://localhost:9901")
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			envoyConfig, err := ParseEnvoyConfig(configDump)
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			envoyConfig.Boostrap.Bootstrap.Node = nil
+
+			compareBootstrap(t, test.expectedOutput, envoyConfig.Boostrap.Bootstrap)
+		})
+	}
+}
