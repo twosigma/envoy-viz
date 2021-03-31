ec0465028079972242ac832f48026273a0e7b971
Ben Levy <ben.levy@twosigma.com>
2021-03-31 09:51:03 -0400

    Set up cli to produce a graph for --admin option

--- envoy-viz.go
+++ envoy-viz.go
@@ -6,6 +6,7 @@ import (
 	"log"
 	"os"
 
+	v3 "github.com/envoyproxy/go-control-plane/envoy/config/bootstrap/v3"
 	"github.com/spf13/pflag"
 	"github.com/twosigma/envoy-viz/configreader"
 	"github.com/twosigma/envoy-viz/graph"
@@ -22,36 +23,37 @@ func main() {
 	if *file != "" && *adminUrl != "" {
 		log.Fatal("Only one of --file or --admin can be set at the same time")
 	}
+	var bs *v3.Bootstrap
 	if *file != "" {
-		bs, err := configreader.FromFile(*file)
+		bootstrap, err := configreader.FromFile(*file)
 		if err != nil {
 			panic(err)
 		}
-		if *render == "" {
-			json.NewEncoder(os.Stdout).Encode(&bs)
-		} else {
-			g, err := graph.BuildGraph(bs)
-			if err != nil {
-				panic(err)
-			}
-			b, err := graph.Render(g, *render)
-			if err != nil {
-				panic(err)
-			}
-			fmt.Println(string(b))
+		bs = bootstrap
+	} else if *adminUrl != "" {
+		r, err := configreader.ReadEnvoyConfig(*adminUrl)
+		if err != nil {
+			panic(err)
 		}
-		return
+		ec, err := configreader.ParseEnvoyConfig(r)
+		if err != nil {
+			panic(err)
+		}
+		bs = ec.Boostrap.Bootstrap
+	} else {
+		panic("Must either set --file or --admin option")
 	}
-	r, err := configreader.ReadEnvoyConfig(*adminUrl)
-	if err != nil {
-		panic(err)
+	if *render == "" {
+		json.NewEncoder(os.Stdout).Encode(&bs)
+	} else {
+		g, err := graph.BuildGraph(bs)
+		if err != nil {
+			panic(err)
+		}
+		b, err := graph.Render(g, *render)
+		if err != nil {
+			panic(err)
+		}
+		fmt.Println(string(b))
 	}
-	ec, err := configreader.ParseEnvoyConfig(r)
-	if err != nil {
-		panic(err)
-	}
-	if ec != nil {
-		json.NewEncoder(os.Stdout).Encode(&ec)
-	}
-
 }
